86f06e562c9d61ed4372aa2c98acf6b62096047b
Mukul <mukul1singh23@gmail.com>
2024-08-10 08:30:40 +0530

    test

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user');
 const MongoStore = require('connect-mongo');
-const dotenv=require('dotenv').config()
+const User = require('./models/user');
+const dotenv = require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log('DB Connected'))
     .catch((err) => console.log(err));
 
@@ -23,78 +26,66 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-
-const mongooseConnection = mongoose.connection;
-
-
-
 const sessionConfig = {
-    secret: 'weneedsomebettersecret',
+    secret: process.env.SESSION_SECRET || 'weneedsomebettersecret',
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         httpOnly: true,
-        // expires: Date.now() + 1000* 60 * 60 * 24 * 7,
-        maxAge:1000* 60 * 60 * 24 * 7 * 1
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
     },
-    store: new MongoStore({ 
-    mongoUrl: process.env.MONGODB_URI, 
-    mongooseConnection,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }), // Use MongoDB to store sessions
-}  
-
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+};
 
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// passport ke ander passport-locol-mongoose ka function bhej rha hu iske jaga apna be bhej sekta hu
+// Passport configuration
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
 
-
-// passport  check krega username and password using authenticate method provided by the passport-local-mongoose package
-passport.use(new LocalStrategy(User.authenticate())); 
-
-
+// Middleware to set local variables
 app.use((req, res, next) => {
-    
-    res.locals.currenturl='/products';
+    res.locals.currenturl = '/products';
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
-
-    // console.log(res.locals.currentUser);
     next();
-})
-
-console.log("appjs wala middleware");
-console.log(session);
+});
 
 // Routes
+app.get('/', (req, res) => {
+    res.send("hello ji");
+});
 
-app.get('/',(req,res)=>{
-    res.send("hello ji")
-})
+// Uncomment and define your routes here
 // const productRoutes = require('./routes/product');
 // const reviewRoutes = require('./routes/review');
-// const authRouts = require('./routes/auth');
-// const api=require('./routes/api');
-// const cart=require('./routes/cart');
+// const authRoutes = require('./routes/auth');
+// const apiRoutes = require('./routes/api');
+// const cartRoutes = require('./routes/cart');
 
-// app.use(authRouts);
+// app.use(authRoutes);
 // app.use(productRoutes);
 // app.use(reviewRoutes);
-// app.use(api);
-// app.use(cart);
+// app.use(apiRoutes);
+// app.use(cartRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).render('error', { err });
+});
 
-const port = 5000;
-
+// Start server
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server running at port ${port}`);
+    console.log(`Server running at port ${port}`);
 });
